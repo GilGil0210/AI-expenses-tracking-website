eb3557e2f4a735988d4bf6ad481f0c38302d10b8
gilbert <Hoyin0210@gmail.com>
2026-07-19 01:43:59 +0800

    updated transaction page

---
+++ transactions.js
@@ -0,0 +1,129 @@
+let transactions = JSON.parse(localStorage.getItem("transactions")) || [];
+if (transactions.length <= 0){
+    transactions = [];
+    localStorage.setItem(
+        "transactions", JSON.stringify(transactions)
+    )
+}
+function saveTransactions(){
+
+    localStorage.setItem(
+
+        "transactions",
+
+        JSON.stringify(transactions)
+
+    );
+
+}
+const table = document.getElementById("transactionTable");
+
+const overlay = document.getElementById("overlay");
+
+const addButton = document.querySelector(".add");
+
+const saveButton = document.getElementById("saveBtn");
+
+const cancelButton = document.getElementById("cancelBtn");
+function displayTransactions() {
+
+    // Remove old rows
+    table.innerHTML = "";
+
+    // Go through every transaction
+    transactions.forEach(function(transaction) {
+
+        // Create a new table row
+        const row = document.createElement("tr");
+
+        // Put HTML inside the row
+        row.innerHTML = `
+
+            <td>${transaction.merchant}</td>
+
+            <td>${transaction.category}</td>
+
+            <td>${transaction.date}</td>
+
+            <td class="${transaction.amount < 0 ? "expense" : "income"}">
+
+                $${transaction.amount.toFixed(2)}
+
+            </td>
+
+            <td>
+
+                <button class="editBtn">✏️</button>
+
+                <button class="deleteBtn">🗑️</button>
+
+            </td>
+
+        `;
+
+        table.appendChild(row);
+        
+
+    });
+    attachDeleteButtons();
+
+}
+function attachDeleteButtons(){
+
+    const buttons=document.querySelectorAll(".deleteBtn");
+
+    buttons.forEach(function(button){
+
+        button.addEventListener("click",function(){
+
+            const index=this.dataset.index;
+
+            transactions.splice(index,1);
+            saveTransactions();
+            displayTransactions();
+
+        });
+
+    });
+
+}
+addButton.addEventListener("click",function(){
+
+    overlay.style.display="flex";
+
+});
+
+cancelButton.addEventListener("click",function(){
+
+    overlay.style.display="none";
+
+});
+
+saveButton.addEventListener("click",function(){
+
+    const merchant=document.getElementById("merchant").value;
+
+    const category=document.getElementById("category").value;
+
+    const date=document.getElementById("date").value;
+
+    const amount=Number(document.getElementById("amount").value);
+
+    transactions.push({
+
+        merchant,
+
+        category,
+
+        date,
+
+        amount
+
+    });
+    saveTransactions();
+    displayTransactions();
+
+    overlay.style.display="none";
+
+});
+displayTransactions();
